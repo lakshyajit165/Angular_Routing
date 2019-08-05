4b7ce853d52f3d485aa9458f2fa65ca049ef85ef
Lakshyajit <lakshyajit165@gmail.com>
2019-08-05 17:53:29 +0530

    Wildcard routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/departments', pathMatch: 'full'},
+  {path: 'departments',component: DepartmentListComponent},
+  {path: 'employees', component: EmployeeListComponent},
+  {path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [DepartmentListComponent, EmployeeListComponent, PageNotFoundComponent];
